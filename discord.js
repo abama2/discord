2c6e12146606f69fc4a52ba513957bdf4a3b0f1d
abama <77401523+abama2@users.noreply.github.com>
2023-12-03 13:41:35 +0500

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,12 +1,18 @@
+// Изменяем атрибут class
+let s = setInterval(function s(){
 const htmlElement = document.querySelector('html');
 htmlElement.setAttribute('class', 'custom-theme-background reduce-motion theme-dark platform-web font-size-16');
 
-// Ñîçäàåì íîâûé ñòèëü
+const titleElement = document.querySelector('title');
+titleElement.textContent = `abama discord`
+
+// Создаем новый стиль
 const styleElement = document.createElement('style');
 styleElement.setAttribute('data-client-themes', 'true');
 styleElement.setAttribute('data-rh', 'true');
 styleElement.textContent = '.custom-theme-background { --custom-theme-background: linear-gradient(64.92deg, var(--bg-gradient-crimson-moon-1) 16.17%, var(--bg-gradient-crimson-moon-2) 72%); }';
 
-// Äîáàâëÿåì íîâûé ñòèëü â ðàçäåë head
+// Добавляем новый стиль в раздел head
 const headElement = document.querySelector('head');
 headElement.appendChild(styleElement);
+},200);
