4b1e441bac1153bf6e8873750c2a7c5879c844d7
abama <77401523+abama2@users.noreply.github.com>
2023-12-03 14:21:13 +0500

    Update discord.js
    Фото баннера

--- discord.js
+++ discord.js
@@ -1,18 +1,26 @@
-// Изменяем атрибут class
-let s = setInterval(function s(){
-const htmlElement = document.querySelector('html');
-htmlElement.setAttribute('class', 'custom-theme-background reduce-motion theme-dark platform-web font-size-16');
+let s = setInterval(function() {
+  const htmlElement = document.querySelector('html');
+  htmlElement.setAttribute('class', 'custom-theme-background reduce-motion theme-dark platform-web font-size-16');
 
-const titleElement = document.querySelector('title');
-titleElement.textContent = `abama discord`
+  const titleElement = document.querySelector('title');
+  titleElement.textContent = `abama discord`
 
-// Создаем новый стиль
-const styleElement = document.createElement('style');
-styleElement.setAttribute('data-client-themes', 'true');
-styleElement.setAttribute('data-rh', 'true');
-styleElement.textContent = '.custom-theme-background { --custom-theme-background: linear-gradient(64.92deg, var(--bg-gradient-crimson-moon-1) 16.17%, var(--bg-gradient-crimson-moon-2) 72%); }';
+  // Создаем новый стиль
+  const styleElement = document.createElement('style');
+  styleElement.setAttribute('data-client-themes', 'true');
+  styleElement.setAttribute('data-rh', 'true');
+  styleElement.textContent = '.custom-theme-background { --custom-theme-background: linear-gradient(64.92deg, var(--bg-gradient-crimson-moon-1) 16.17%, var(--bg-gradient-crimson-moon-2) 72%); }';
 
-// Добавляем новый стиль в раздел head
-const headElement = document.querySelector('head');
-headElement.appendChild(styleElement);
-},200);
+  // Добавляем новый стиль в раздел head
+  const headElement = document.querySelector('head');
+  headElement.appendChild(styleElement);
+
+  // фото баннера
+  var divElement = document.querySelector('.banner__6d414.popoutBanner__9f5b9');
+  if (divElement) {
+    if (!divElement.style.backgroundImage) {
+      divElement.style.backgroundColor = 'rgb(210, 179, 25)';
+      divElement.style.backgroundImage = 'url("/assets/56d360a7672d87843eaa.png")';
+    }
+  }
+}, 200);
