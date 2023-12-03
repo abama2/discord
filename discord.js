5d8ed5482c44e97ceb546575cfb77e7c2c1cfdbb
abama <77401523+abama2@users.noreply.github.com>
2023-12-03 12:57:24 +0500

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,13 +1,12 @@
-// Изменяем атрибут class
 const htmlElement = document.querySelector('html');
 htmlElement.setAttribute('class', 'custom-theme-background reduce-motion theme-dark platform-web font-size-16');
 
-// Создаем новый стиль
+// Г‘Г®Г§Г¤Г ГҐГ¬ Г­Г®ГўГ»Г© Г±ГІГЁГ«Гј
 const styleElement = document.createElement('style');
 styleElement.setAttribute('data-client-themes', 'true');
 styleElement.setAttribute('data-rh', 'true');
 styleElement.textContent = '.custom-theme-background { --custom-theme-background: linear-gradient(64.92deg, var(--bg-gradient-crimson-moon-1) 16.17%, var(--bg-gradient-crimson-moon-2) 72%); }';
 
-// Добавляем новый стиль в раздел head
+// Г„Г®ГЎГ ГўГ«ГїГҐГ¬ Г­Г®ГўГ»Г© Г±ГІГЁГ«Гј Гў Г°Г Г§Г¤ГҐГ« head
 const headElement = document.querySelector('head');
 headElement.appendChild(styleElement);
