8c525c00cf33eed2681e412826d03cedcc84cd11
abama <77401523+abama2@users.noreply.github.com>
2023-12-03 15:08:24 +0500

    Update discord.js
    optimase

--- discord.js
+++ discord.js
@@ -1,30 +1,31 @@
-let s = setInterval(function() {
+const updateUI = () => {
+  // Устанавливаем класс в элементе <html>
   const htmlElement = document.querySelector('html');
-  htmlElement.setAttribute('class', 'custom-theme-background reduce-motion theme-dark platform-web font-size-16');
+  htmlElement.classList.add('custom-theme-background', 'reduce-motion', 'theme-dark', 'platform-web', 'font-size-16');
 
+  // Обновляем текст в элементе <title>
   const titleElement = document.querySelector('title');
-  titleElement.textContent = `abama discord`
+  titleElement.textContent = 'abama discord';
 
-  // Создаем новый стиль
+  // Создаем стиль
   const styleElement = document.createElement('style');
   styleElement.setAttribute('data-client-themes', 'true');
   styleElement.setAttribute('data-rh', 'true');
   styleElement.textContent = '.custom-theme-background { --custom-theme-background: linear-gradient(64.92deg, var(--bg-gradient-crimson-moon-1) 16.17%, var(--bg-gradient-crimson-moon-2) 72%); }';
 
-  // Добавляем новый стиль в раздел head
+  // Добавляем стиль в раздел <head>
   const headElement = document.querySelector('head');
   headElement.appendChild(styleElement);
 
-  // фото баннера
-  var divElement = document.querySelector('.banner__6d414.popoutBanner__9f5b9');
-  if (divElement) {
-    if (!divElement.style.backgroundImage) {
-      divElement.style.backgroundColor = 'rgb(210, 179, 25)';
-      divElement.style.backgroundImage = 'url("/assets/56d360a7672d87843eaa.png")';
-    }
+  // Обновляем фото баннера
+  const divElement = document.querySelector('.banner__6d414.popoutBanner__9f5b9');
+  if (divElement && !divElement.style.backgroundImage) {
+    divElement.style.backgroundColor = 'rgb(210, 179, 25)';
+    divElement.style.backgroundImage = 'url("/assets/56d360a7672d87843eaa.png")';
   }
-  // украшение (бетта)
-    const avatarStackElement = document.querySelector('.avatarStack__6604a');
+
+  // Добавляем украшение (бетта)
+  const avatarStackElement = document.querySelector('.avatarStack__6604a');
   if (avatarStackElement) {
     const imgElement = document.createElement('img');
     imgElement.setAttribute('class', 'avatar__991e2');
@@ -33,4 +34,7 @@ let s = setInterval(function() {
     imgElement.setAttribute('aria-hidden', 'true');
     avatarStackElement.appendChild(imgElement);
   }
-}, 250);
+};
+
+// Вызываем функцию обновления интерфейса каждые 250 миллисекунд
+const sw = setInterval(updateUI, 250);
